e7d157d04f1a2840288dea7d611d55e5b348bf92
Sophia <pricesophia.dev@gmail.com>
2020-06-20 18:12:03 +0100

    Add avatar

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100.0,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/bitmoji.png'),
+                backgroundColor: Colors.white,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue,
-              )
             ],
           ),
         ),
